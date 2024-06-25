fa15ab6c013f23b9f78877966265164ec435b650
Robin Ince <robince@gmail.com>
2024-06-25 16:23:33 -0400

    add readme and cleanup examples

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Improving accessibility by automatically generating alt-text for images
+
+## FabData LLM
+
+This project uses the [FabData LLM](https://github.com/AI-for-Education/fabdata-llm) package, which provides a convenient chatbot-style API to a range of LLM models. You can find an overview of the package in the project [README](https://github.com/AI-for-Education/fabdata-llm/blob/main/README.md). This is provided as a framework with the tooling necessary to quickly start working on the problem, but please feel free to use other LLM frameworks if you prefer or are familiar with them. 
+
+## Data and Example
+
+You can use whatever content you like from OER Commons. For example there is a large quantity of online courseware. You can use the metadata database from the other projects to investigate this if you like. However, to get you started we have included a number of [Flexbooks](https://www.ck12.org/fbbrowse/) from the CK-12 Foundation, which are textbooks curated from OERCommons hosted content. You can find the available Flexbooks in PDF format [here](https://www.dropbox.com/scl/fo/y67u4dgtdcc8b8qxqsr4m/AK_l7I3-Ac_lXiIbKX6qzqw?rlkey=0bm04kvv1od48xgugwa1m6r2j&dl=0). Download the `Flexbooks` folder and put it in a `data` sub-folder of this project. 
+
+## Possible Tasks
+
+- How to evaluate the generated alt-texts from different models and different prompts? What makes a good alt-text, and how does it differ from the captions the images currently have in the Flexbooks? Does this depend on the area (maybe scientific diagrams have different requirements in terms of useful level of detail than illustrations in a social science text). Is there a trade-off between conciseness vs detail, and what is better for accessibility? Is our idea of alt-text limited by current implementations (i.e. html alt-text). How could this be improved? Perhaps there could be two levels of description, concise (~20 words) and a more detailed description (>100 words).
+- How can the prompts used for this task be optimized? Are the outputs improved by including more context, i.e. the title of that chapter or the abstract, goals and audience of that book? What shoudl we ask the model to do to optimize accessibility?
+- Can we make a minimum viable product using this approach? ie convert the pdf content to a website with alt-text, perhaps with different levels of conciseness (buttons? mouseover?)
+- Could you make a webapp (using streamlit or some other approach) that takes an arbitrary content page from the web, and produces a copied version with alt-text added. 
---
+++ scripts/example_alttext.py
@@ -0,0 +1,96 @@
+# %%
+from io import BytesIO
+from pathlib import Path
+
+import fitz
+from dotenv import load_dotenv
+from fdllm import get_caller
+from fdllm.chat import ChatController
+from PIL import Image
+from tqdm import tqdm
+
+from project import DATADIR
+
+load_dotenv(override=True)
+
+# %% Read images from a specified PDF
+testfile = (
+    DATADIR
+    / "Flexbooks"
+    / "Earth Science"
+    / "CK-12-Earth-Science-For-Middle-School_b_v1_smr.pdf"
+)
+f = fitz.open(testfile)
+npages = f.page_count
+text = []
+images = []
+# load the text and images from each page of the pdf
+for pagen in tqdm(range(npages)):
+    page = f.load_page(pagen)
+    text_ = page.get_text()
+    image_list = page.get_images(full=True)
+    images_ = [
+        Image.open(BytesIO(f.extract_image(img_[0])["image"])) for img_ in image_list
+    ]
+
+    text.append(text_)
+    images.append(images_)
+
+# %%
+# select an image-capable OpenAI model (any of these)
+model = "gpt-4o"
+# model = "gpt-4-turbo"
+# model = "gpt-4-vision-preview"
+
+# this instantiates a chat with the selected model
+chatter = ChatController(Caller=get_caller(model))
+
+# lets start with the simplest possible prompt
+prompt = "Please describe these images"
+# here we call the model passing in the image from page 16
+inmsg, outmsg = chatter.chat(prompt, images=images[15])
+print(outmsg.Message)
+images[15][0]
+
+# %% try a prompt to get a more concise description more suitable for an alt-text tag
+chatter = ChatController(Caller=get_caller("gpt-4o"))
+# here we suggest 20 words
+prompt = """
+Please describe these images for an alt-text to enable a blind person to
+understand what is shown in the image. The description should be less than 20
+words per image.
+""".replace( "\n", " ").strip()
+inmsg, outmsg = chatter.chat(
+    prompt,
+    images=images[15],
+)
+print(outmsg.Message)
+images[15][0]
+
+# %% 
+# maybe 20 words is a bit too concise, here we try a different model with more words
+chatter = ChatController(Caller=get_caller("gpt-4-turbo"))
+max_words = 60
+prompt = f"""
+Please describe these images for an alt-text to enable a blind person to
+understand what is shown in the image. The description should be less than {max_words} 
+words per image.
+""".replace( "\n", " ").strip()
+inmsg, outmsg = chatter.chat(
+    prompt,
+    images=images[15],
+)
+print(outmsg.Message)
+images[15][0]
+
+# %%
+# other parameters of interest:
+# ChatController.chat function:
+# detail = 'low' , 'high' (default 'low'). This sets the image resolution for OpenAI
+
+# ChatController init:
+#    Sys_Msg={
+#        0: "This will appear at the start of the conversation"
+#        -1: "This will appear at the end of the conversation, after the user chat input"
+#        -2: "This will appear at the end of the conversation, before the user chat input"
+#    }
